433ac4c741a42ce53d9a5b9c03c0a45245619917
timbo <timbodd1996@gmail.com>
2021-11-23 14:22:07 +0100

    Initial Commit

---
+++ src/main/java/VerteilteSystemeAPI/VerteilteSysteme/VerteilteSystemeApplication.java
@@ -0,0 +1,13 @@
+package VerteilteSystemeAPI.VerteilteSysteme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerteilteSystemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerteilteSystemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/VerteilteSystemeAPI/VerteilteSysteme/VerteilteSystemeApplicationTests.java
@@ -0,0 +1,13 @@
+package VerteilteSystemeAPI.VerteilteSysteme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerteilteSystemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
